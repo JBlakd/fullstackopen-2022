90a72321ecab1979ddad9e58d42d69f94bb05fc0
Ivan Hu <ivanhu2246@gmail.com>
2022-07-16 23:09:51 +0800

    3.6 complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     "id": 1,
@@ -28,8 +30,50 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (request, response) => {
+  response.send(`<div>Phonebook has info for ${persons.length} people</div> </br> <div>${new Date()}</div>`)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  // console.log("GET specific person response: ", person)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const inputPerson = request.body
+
+  if (!inputPerson.hasOwnProperty("name") || !inputPerson.hasOwnProperty("number")) {
+    response.status(400).json({ error: 'malformed person: name or number is missing' })
+    return
+  }
+
+  if (persons.find(p => p.name === inputPerson.name)) {
+    response.status(400).json({ error: "name must be unique" })
+    return
+  }
+
+  const acceptablePerson = inputPerson
+  acceptablePerson.id = Math.floor(Math.random() * 100000000);
+  persons = persons.concat(acceptablePerson)
+
+  response.json(acceptablePerson)
 })
 
 const PORT = 3001
