8dfb437a940fd5ec4ff81ec5e2538b7f832cb875
Ivan Hu <ivanhu2246@gmail.com>
2022-07-17 23:42:18 +0800

    4.4 complete

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return (
+    blogs
+      .reduce((acc, cur) => acc + cur.likes, 0)
+  )
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
