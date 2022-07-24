f3f17846a2851caf25668764f9045bf813fed978
Ivan Hu <ivanhu2246@gmail.com>
2022-07-24 23:20:30 +0800

    5.7 complete

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,48 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [isShown, setIsShown] = useState(false)
+
+  const toggleIsShown = () => {
+    setIsShown(!isShown)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (isShown) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author} <button onClick={toggleIsShown}>hide</button>
+        </div>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>
+          {blog.user.name}
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author} <button onClick={toggleIsShown}>view</button>
+        </div>
+      </div>
+    )
+  }
+
+
+}
 
 export default Blog
