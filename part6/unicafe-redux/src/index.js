105579f3ec680a50249b19887885de907166095f
Ivan Hu <ivanhu2246@gmail.com>
2022-07-26 20:43:19 +0800

    6.2 complete

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  const total = Object.values(store.getState()).reduce((acc, cur) => acc + cur)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={zero}>reset stats</button>
+      <h1>statistics</h1>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+      <div>all {total}</div>
+      <div>average {(store.getState().good - store.getState().bad) / total}</div>
+      <div>average {store.getState().good * 100 / total}%</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
