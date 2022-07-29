266272088870a8e101e2becc850aa7a1fb581d55
Ivan Hu <ivanhu2246@gmail.com>
2022-07-29 23:37:13 +0800

    prior to 9.15

--- part9/courseinfo/src/App.tsx
+++ part9/courseinfo/src/App.tsx
@@ -5,33 +5,51 @@ const Header = ({ name }: { name: string }): JSX.Element => {
   )
 }
 
-interface CourseProps {
-  name: string,
-  exerciseCount: number
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
 }
 
-const Course = ({ name, exerciseCount }: CourseProps): JSX.Element => {
+interface CourseNormalPart extends CoursePartBase {
+  type: "normal";
+  description: string;
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: "submission";
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
+const Course = ({ name, exerciseCount, type }: CoursePartBase): JSX.Element => {
   return (
     <p>
-      {name} {exerciseCount}
+      {name} {exerciseCount} {type}
     </p>
   )
 }
 
-const Content = ({ parts }: { parts: Array<CourseProps> }): JSX.Element => {
+const Content = ({ parts }: { parts: Array<CoursePartBase> }): JSX.Element => {
   const generateKey = () => {
     return Math.random().toString(36).substring(2, 7);
   }
 
   return (
     <div>
-      {parts.map(p => <Course key={generateKey()} name={p.name} exerciseCount={p.exerciseCount} />)}
+      {parts.map(p => <Course key={generateKey()} name={p.name} exerciseCount={p.exerciseCount} type={p.type} />)}
     </div>
 
   )
 }
 
-const Total = ({ courses }: { courses: Array<CourseProps> }): JSX.Element => {
+const Total = ({ courses }: { courses: Array<CoursePartBase> }): JSX.Element => {
   return (
     <p>
       Number of exercises{" "}
@@ -42,20 +60,33 @@ const Total = ({ courses }: { courses: Array<CourseProps> }): JSX.Element => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
     }
-  ];
+  ]
 
   return (
     <div>
