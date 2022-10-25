2699c22d4c41a618522ad30d9e43210dcfac7000
Ivan Hu <47449822+JBlakd@users.noreply.github.com>
2022-10-25 14:46:29 +1100

    7.2 complete

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Routes, Route, Link
+  Routes, Route, Link, useParams
 } from "react-router-dom"
 
 const Menu = ( { anecdotes, addNew }) => {
@@ -18,6 +18,7 @@ const Menu = ( { anecdotes, addNew }) => {
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes}/>} />
         <Route path='/create' element={<CreateNew addNew={addNew}/>} />
         <Route path='/about' element={<About/>} />
+        <Route path='/:id' element={<Anecdote anecdotes={anecdotes}/>} />
       </Routes>
     </Router>
   )
@@ -27,10 +28,27 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/${anecdote.id}`}>
+            {anecdote.content}
+          </Link>
+        </li>)}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id;
+  const anecdote = anecdotes.find(a => a.id === Number(id));
+
+  return (
+    <div>
+      <h1>{anecdote.content} by {anecdote.author}</h1>
+      <p>has {anecdote.votes} votes</p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
