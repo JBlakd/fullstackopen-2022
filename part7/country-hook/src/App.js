d5a984234dd597239088d0c90e0372bfd3dea11b
Ivan Hu <47449822+JBlakd@users.noreply.github.com>
2022-10-26 13:09:42 +1100

    7.7 complete

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,88 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  };
+
+  return {
+    type,
+    value,
+    onChange
+  };
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    const getCountry = async (name) => {
+      const res = await axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`);
+      // console.log("res: ", res);
+      if (res.status !== 200) {
+        setCountry(null);
+        return;
+      }
+
+      const data = res.data[0];
+      setCountry({
+        name: data.name,
+        population: data.population,
+        capital: data.capital[0],
+        flags: data.flags
+      })
+    } 
+
+    if (name === '') {
+      return;
+    }
+
+    getCountry(name);
+  }, [name]);
+
+  return country;
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return <div>not found...</div>;
+  }
+
+  console.log('country: ', country);
+
+  return (
+    <div>
+      <h3>{country.name.common}</h3>
+      <div>population {country.population}</div> 
+      <div>capital {country.capital}</div>
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+    </div>
+  )  
+}
+
+const App = () => {
+  const nameInput = useField('text');
+  const [name, setName] = useState('');
+  const country = useCountry(name);
+
+  const fetch = (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App;
