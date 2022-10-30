3d69477ae08755f36a52255ff494456f6fbd3c54
Ivan Hu <47449822+JBlakd@users.noreply.github.com>
2022-10-31 10:38:29 +1100

    8.1 complete

---
+++ part8/graphql-server/utils/array-utils.js
@@ -0,0 +1,9 @@
+// const uniqueItems = (list, key) => {
+//     const reduced = list.reduce((objSet, curObj) => objSet.add(list[key]), new Set)
+//     console.log('reduced', reduced)
+//     return reduced.size
+// }
+
+// module.exports = {
+//     uniqueItems
+// }
